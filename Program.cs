69ff6da8970403802651c2e1cdaeaaea96c8092f
OliverMengich <oliverkemei2000@gmail.com>
2019-10-15 20:46:28 +0300

    Wonderr

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_gitSoln
+{
+    class Program
+    {
+        double lenght = 30.5d;
+        double width = 15.6d;
+        static void Main(string[] args)
+        {
+           Program p1 = new Program();
+           p1.Display();
+        }
+        public void Display()
+        {
+            Program p = new Program();
+            System.Console.WriteLine($"the length is {p.lenght}");
+            System.Console.WriteLine($"the width is {p.width}");
+        }
+    }
+}
